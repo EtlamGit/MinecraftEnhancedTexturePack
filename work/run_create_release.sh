469cb25a57d94837b3e68092d93e2e2b4b803c3e
EtlamGit <etlam@nurfuerspam.de>
2015-10-17 13:18:27 +0200

    added resolution hint to pack.png
    now all pack.png icons have the corresponding resolution scale in lewer
    right corner
    fix #15

--- work/run_create_release.sh
+++ work/run_create_release.sh
@@ -9,11 +9,11 @@ do
 
   cd ${SCALE}
 
-  zip ../MCE_${SCALE}_Blocks_${VER}.zip -@ < ../work/release_BLOCKS_files.lst
-  zip ../MCE_${SCALE}_Items_${VER}.zip -@  < ../work/release_ITEMS_files.lst
-  zip ../MCE_${SCALE}_Mobs_${VER}.zip -@   < ../work/release_MOBS_files.lst
-  zip ../MCE_${SCALE}_GUI_${VER}.zip -@    < ../work/release_GUI_files.lst
-  zip ../MCE_${SCALE}_Base_${VER}.zip -@   < ../work/release_BASE_files.lst
+  zip ../ME_${SCALE}_Blocks_${VER}.zip -@ < ../work/release_BLOCKS_files.lst
+  zip ../ME_${SCALE}_Items_${VER}.zip -@  < ../work/release_ITEMS_files.lst
+  zip ../ME_${SCALE}_Mobs_${VER}.zip -@   < ../work/release_MOBS_files.lst
+  zip ../ME_${SCALE}_GUI_${VER}.zip -@    < ../work/release_GUI_files.lst
+  zip ../ME_${SCALE}_Base_${VER}.zip -@   < ../work/release_BASE_files.lst
 
   cd ..
 
