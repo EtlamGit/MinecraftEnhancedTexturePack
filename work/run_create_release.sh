2752bd8e4a494ca0d4c0ca54b8ad1590c796d092
EtlamGit <etlam@nurfuerspam.de>
2016-02-29 19:26:13 +0100

    upgrade pack.mcmeta version
    fixes #44
    first setup to generate 1.9 resource packs (some GUI elements are still
    wrong)

--- work/run_create_release.sh
+++ work/run_create_release.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
-VER=1.7-`date +%Y-%m-%d`
+#VER=1.7-`date +%Y-%m-%d`
+#VER=1.8-`date +%Y-%m-%d`
+VER=1.9-`date +%Y-%m-%d`
+
 
 cd ..
 
+
 for SCALE in 32x 64x 96x 128x 256x
 do
-
   cd ${SCALE}
 
   zip ../ME_${SCALE}_Blocks_${VER}.zip -@ < ../work/release_BLOCKS_files.lst
   zip ../ME_${SCALE}_Items_${VER}.zip -@  < ../work/release_ITEMS_files.lst
   zip ../ME_${SCALE}_Mobs_${VER}.zip -@   < ../work/release_MOBS_files.lst
-  zip ../ME_${SCALE}_GUI_${VER}.zip -@    < ../work/release_GUI_files.lst
   zip ../ME_${SCALE}_Base_${VER}.zip -@   < ../work/release_BASE_files.lst
 
   cd ..
-
 done
+
+
+for SCALE in 32x 64x
+do
+  cd ${SCALE}
+
+  zip ../ME_${SCALE}_GUI_${VER}.zip -@    < ../work/release_GUI_files.lst
+
+  cd ..
+done
+
 
 cd work
--- work/run_image_identify.sh
+++ work/run_image_identify.sh
@@ -10,5 +10,6 @@ find ./256x -name *.png -type f -exec identify {} \; > ./work/identify_256.log
 
 find ./16x/minecraft_1.7.10 -name *.png -type f -exec identify {} \; > ./work/identify_vanilla_1.7.10.log
 find ./16x/minecraft_1.8.8  -name *.png -type f -exec identify {} \; > ./work/identify_vanilla_1.8.8.log
+find ./16x/minecraft_1.9    -name *.png -type f -exec identify {} \; > ./work/identify_vanilla_1.9.log
 
 cd work
